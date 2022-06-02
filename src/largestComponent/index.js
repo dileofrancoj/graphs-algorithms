2e402e0acde58a056ba10a58a79f09bff250628d
Franco Di Leo <dileo.francoj@gmail.com>
2022-06-02 08:26:38 -0300

    feat: largest path problem from not connected graph

--- src/largestComponent/index.js
+++ src/largestComponent/index.js
@@ -1,1 +1,25 @@
-function largestPath(graph) {}
+const { notConnectedGraph } = require("../models/graphs");
+
+function largestPath(graph) {
+  let longest = 0;
+  const visited = new Set();
+  for (let node in graph) {
+    const size = exploreSize(graph, node, visited);
+    if (size > longest) longest = size;
+  }
+  return longest;
+}
+
+function exploreSize(graph, node, visited) {
+  if (visited.has(node)) return 0;
+  let size = 1;
+  visited.add(node);
+
+  for (let neighbor of graph[node]) {
+    size += exploreSize(graph, neighbor, visited);
+  }
+
+  return size;
+}
+
+console.log(largestPath(notConnectedGraph));
